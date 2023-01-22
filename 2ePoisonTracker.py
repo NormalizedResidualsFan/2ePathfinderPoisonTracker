f91f599788c75ded4244597bdf863d916c14cb21
NormalizedResidualsFan <123308738+NormalizedResidualsFan@users.noreply.github.com>
2023-01-22 09:02:52 -0500

    documentation update

--- 2ePoisonTracker.py
+++ 2ePoisonTracker.py
@@ -3,7 +3,7 @@ This script was made to facilitate tracking various poisons applied to multiple 
 
 It is intended to be run from a terminal with python 3.0 (or newer)
 
-It will print out to terminal a set of poison victims as entered by the user, with remaining round durations and present stage
+It will print out to terminal a set of poison victims as entered by the user, with remaining round durations and present poison stage. Remaining round durations may be decremented individually or all active victims may have their rounds remaining decremented simultaneously
 
 Only common and uncommon injury poisons, as found on 2e.aonprd.com as of 2023-01-22, are included here
 """
